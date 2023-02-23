968aec36a28b54edcda4fa76a569d3caae35f79d
jeongyeon.seo <s9530348@naver.com>
2023-02-23 16:06:35 +0900

    feat: 홈 페이지 마크업 및 스타일드 컴포넌트 설치

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import * as S from "./style";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <nav>네브바</nav>
+      <section>스와이프 이미지</section>
+      <main>
+        <ul>
+          <li>
+            <figure>
+              <img src="" alt="" />
+              <figcaption>
+                <p>
+                  <small>우당탕탕 라이캣의 실험실</small>
+                </p>
+                <p>Hack Your Life 개발자 노트북 파우치</p>
+                <strong>28,000</strong>
+                <span>원</span>
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
