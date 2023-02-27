6072242fe9da6d5c0344e2386193a0d8826323a0
jeongyeon.seo <s9530348@naver.com>
2023-02-27 20:18:35 +0900

    design: styled-reset 설치 및 GlobalStyle 설정 #1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+.ir {
+        position: absolute;
+        clip-path: inset(50%);
+        width:1px;
+        height: 1px;
+        margin:-1px;
+        overflow:hidden;
+}
+
+button{
+    border: 0;
+    cursor: pointer;
+    background-color: transparent;
+}
+
+a {
+    color: black;
+    text-decoration:none;
+}
+`;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App tab="home" />
+  </>
+);
