561838163deca539bb7921ad442e6666ed6616de
jeongyeon.seo <s9530348@naver.com>
2023-02-27 19:51:30 +0900

    design: 홈 페이지 헤더 레이아웃 완성 #1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,21 @@
+import main_logo from "../../assets/images/main_logo.png";
+import * as S from "./style";
+
 function Header() {
-  return <nav>Header</nav>;
+  return (
+    <S.HeaderContainer>
+      <S.HeaderWrapper>
+        <img src={main_logo} alt="메인 로고" />
+        <form>
+          <label className="ir" htmlFor="searchId">
+            상품 검색
+          </label>
+          <input id="searchId" type="text" placeholder="상품을 검색해보세요!" />
+          <button>클릭</button>
+        </form>
+      </S.HeaderWrapper>
+    </S.HeaderContainer>
+  );
 }
 
 export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  background-color: #fff;
+  z-index: 10;
+`;
+
+export const HeaderWrapper = styled.header`
+  position: relative;
+  display: flex;
+  align-items: center;
+  max-width: 100%;
+  height: 90px;
+  padding: 0 auto;
+`;
