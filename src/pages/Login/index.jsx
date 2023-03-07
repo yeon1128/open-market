42c494478e345a8d073f482eb3f9b2e1d3ea9ff8
jeongyeon.seo <s9530348@naver.com>
2023-03-07 10:52:02 +0900

    design: 로그인 페이지 마크업 #6

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <form>
+      <fieldset>
+        <legend>로그인</legend>
+        <label htmlFor="userId">사용자 아이디</label>
+        <input type="text" id="userId" required />
+
+        <label htmlFor="userPassword">사용자 비밀번호</label>
+        <input type="password" id="userPassword" required />
+
+        <button type="submit">로그인</button>
+      </fieldset>
+    </form>
+  );
 }
