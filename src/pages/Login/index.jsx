04d72c8e3ccc3efedee3ade918671d34b151cc71
jeongyeon.seo <s9530348@naver.com>
2023-03-07 13:50:39 +0900

    feat: 사용자가 아이디 및 패스워드 입력 시 로그인 버튼 클릭하면 submit 이벤트 발생 #6

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
+  const [userId, setUserId] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const handleData = (e) => {
+    if (e.target.type === "text") {
+      setUserId(e.target.value);
+    } else if (e.target.type === "password") {
+      setUserPassword(e.target.value);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userId, userPassword);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>로그인</legend>
         <label htmlFor="userId">사용자 아이디</label>
-        <input type="text" id="userId" required />
+        <input
+          type="text"
+          id="userId"
+          required
+          value={userId}
+          onChange={handleData}
+        />
 
         <label htmlFor="userPassword">사용자 비밀번호</label>
-        <input type="password" id="userPassword" required />
+        <input
+          type="password"
+          id="userPassword"
+          required
+          value={userPassword}
+          onChange={handleData}
+        />
 
         <button type="submit">로그인</button>
       </fieldset>
