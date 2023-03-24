2c033299ab6fe8ecee5ed7b1d35cbc75155b54b9
jeongyeon.seo <s9530348@naver.com>
2023-03-24 12:53:28 +0900

    refactor: 회원가입 기능을 위한 slice 분리 #7

---
+++ src/feature/registerSlice.ts
@@ -0,0 +1,96 @@
+import { RootState } from "../store";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export interface RegisterPostData {
+  username: string;
+  password: string;
+  password2: string;
+  phone_number: string;
+  name: string;
+}
+interface RegisterState {
+  registerStatus: string;
+  error: string;
+  userType: string;
+  nameStatus: string;
+  nameMessage: string;
+}
+const initialState: RegisterState = {
+  registerStatus: "idle",
+  error: "",
+  userType: "BUYER",
+  nameStatus: "idle",
+  nameMessage: "",
+};
+
+// 회원가입
+export const axiosPostRegister = createAsyncThunk(
+  "register/axiosPostJoinBuyer",
+  async (
+    {
+      userType,
+      userData,
+    }: {
+      userType: string;
+      userData: RegisterPostData;
+    },
+    { rejectWithValue }
+  ) => {
+    const url = `${process.env.REACT_APP_API_KEY}/accounts/signup/`;
+    try {
+      const data = userData;
+      const res = await axios.post(url, data);
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      console.log(err.response.data);
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+// slice 생성
+export const registerSlice = createSlice({
+  name: "registerSlice",
+  initialState,
+  reducers: {
+    resetAll: () => initialState,
+    resetName: (state) => {
+      state.nameStatus = "idle";
+      state.nameMessage = "";
+    },
+    resetRegister: (state) => {
+      state.registerStatus = "idle";
+      state.error = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(axiosPostRegister.pending, (state) => {
+      state.registerStatus = "loading";
+    });
+    builder.addCase(axiosPostRegister.fulfilled, (state) => {
+      state.registerStatus = "succeeded";
+    });
+    builder.addCase(axiosPostRegister.rejected, (state, action) => {
+      state.registerStatus = "failed";
+      if (action.payload) {
+        state.error = Object.values(action.payload as any)
+          .map((message: any) => message.join().toString())
+          .join("\n");
+      } else {
+        state.error =
+          action.error.message || "Something is wrong in Register:<";
+      }
+    });
+  },
+});
+
+export const getNameStatus = (state: RootState) => state.register.nameStatus;
+export const getNameMessage = (state: RootState) => state.register.nameMessage;
+export const getRegisterStatus = (state: RootState) =>
+  state.register.registerStatus;
+export const getRegisterError = (state: RootState) => state.register.error;
+export const getJoinUserType = (state: RootState) => state.register.userType;
+export const { resetName, resetRegister, resetAll } = registerSlice.actions;
+export default registerSlice.reducer;
--- src/store.ts
+++ src/store.ts
@@ -1,30 +1,6 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-
-interface RegisterState {
-  username: string;
-  password: string;
-  password2: string;
-  phone_number: string;
-  name: string;
-}
-const initialState: RegisterState = {
-  username: "cjf",
-  password: "123",
-  password2: "123",
-  phone_number: "1111-111",
-  name: "dfssdf",
-};
-const registerSlice = createSlice({
-  name: "registerSlice",
-  initialState,
-  reducers: {
-    resetAll: () => initialState,
-    resetName: (state) => {
-      state.username = "";
-    },
-  },
-});
+import { registerSlice } from "./feature/registerSlice";
 
 const store = configureStore({
   reducer: {
@@ -33,12 +9,8 @@ const store = configureStore({
 });
 export default store;
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-//hooks.ts
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
